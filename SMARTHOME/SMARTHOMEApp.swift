042faa3835722fb767c93bf3ddc08a08927a3ad8
Alexander Wong <wong.a@etoncollege.org.uk>
2024-11-26 11:17:45 +0000

    Initial Commit

---
+++ SMARTHOME/SMARTHOMEApp.swift
@@ -0,0 +1,17 @@
+//
+//  SMARTHOMEApp.swift
+//  SMARTHOME
+//
+//  Created by Wong, Alex (JD) on 26/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct SMARTHOMEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
